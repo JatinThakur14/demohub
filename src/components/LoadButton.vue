8899a3e370dcdbc58d7545779d7350d1bc43b2f4
JatinThakur <jtkalga@gmail.com>
2022-09-18 11:34:18 +0530

    Changed the color of button

--- src/components/LoadButton.vue
+++ src/components/LoadButton.vue
@@ -1,22 +1,18 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { useCounterStore } from "@/stores/counter";
 const butt = ref<null | { disabled: boolean }>(null);
 const store = useCounterStore();
-
-function loadMoreButton() {
-  console.log(store.limit, store.changes.length);
-  if (store.limit === store.changes.length && butt.value != null) {
-    console.log("Limit Reached");
+watchEffect(() => {
+  if (store.limit === store.changes.length && butt.value != null)
     butt.value.disabled = true;
-  } else store.limit++;
-}
+});
 </script>
 <template>
-  <button ref="butt" class="button" @click="loadMoreButton">LOAD MORE</button>
+  <button ref="butt" class="button" @click="store.limit++">LOAD MORE</button>
 </template>
 <style scoped lang="scss">
-.button {
+button {
   width: 8rem;
   height: 2.5rem;
   color: #6c6c6c;
@@ -24,6 +20,9 @@ function loadMoreButton() {
   font-size: 16px;
   font-weight: 700;
   background-color: transparent;
-  border: 4px solid rgba(27, 31, 36, 0.5);
+  border: 4px solid /*#00f5*/ #a9c9ea;
+  &:disabled {
+    border-color: grey;
+  }
 }
 </style>
